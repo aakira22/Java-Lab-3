fa812a7ad7c4adfce989446544d34c53f21a690a
Arthur Diwa <arthurakira.diwa@gcash.com>
2022-03-12 15:00:46 +0800

    commit

--- src/abstractFactory/FoodProducer.java
+++ src/abstractFactory/FoodProducer.java
@@ -1,10 +1,12 @@
 package abstractFactory;
 
 public class FoodProducer {
-    public static AbstractFactory getFactory(boolean fried);
-        if(fried){
-        return new FriedFactory();
-    }else{
-        return new GrilledFactory();
+    public static AbstractFactory getFactory(boolean fried) {
+
+        if (fried) {
+            return new FriedFactory();
+        } else {
+            return new GrilledFactory();
+        }
     }
 }
